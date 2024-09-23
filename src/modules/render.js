f3e63d79f70043620d2d3027f3dff6aea85521c4
Степан <19stepan94@mail.ru>
2024-09-23 12:09:51 +0500

    добавил функуцию открытия меню

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,8 +1,36 @@
-import {createModal} from './create.js';
+import {createModal, openNavWindow} from './create.js';
 import {formSubmit} from './control.js';
 
 export const renderModal = () => {
   const {modal, form, closeBtn} = createModal();
+
+  const closeModal = () => {
+    form.reset();
+    modal.classList.add('modal__hide');
+    setTimeout(() => {
+      document.body.removeChild(modal);
+    }, 400);
+  };
+
+  document.body.appendChild(modal);
+  modal.classList.add('modal__show');
+
+  closeBtn.addEventListener('click', closeModal);
+  window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formSubmit(form);
+    closeModal();
+  });
+};
+
+export const renderMenu = () => {
+  const {navWindow, navContent} = openNavWindow();
 
   const closeModal = () => {
     form.reset();
