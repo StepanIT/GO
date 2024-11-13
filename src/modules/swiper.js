85b56c1b258ca71f63bedc0bfa31f9b48707f7d8
Степан Рудаков <19stepan94@mail.ru>
2024-11-13 21:05:58 +0500

    сделал слайдер

---
+++ src/modules/swiper.js
@@ -0,0 +1,27 @@
+new Swiper('.swiper', {
+  slidesPerView: 3,
+  loop: true,
+
+
+  navigation: {
+    nextEl: '.halls__list-right',
+    prevEl: '.halls__list-left',
+  },
+
+  mousewheel: true,
+  keyboard: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    660: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    970: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
