135d31634742e5955d4d12bd2aa55f2ad504b374
Степан Рудаков <19stepan94@mail.ru>
2024-11-14 23:07:30 +0500

    commit

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,11 @@
 import './acc.js';
 import './swiper.js';
 import './validate.js';
+import './flatpickr.js';
 import {generalListeners} from './control.js';
+import { initFlatpickr } from './flatpickr.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   generalListeners();
+  initFlatpickr();
 });
